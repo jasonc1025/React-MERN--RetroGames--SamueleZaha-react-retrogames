961a26f0bceaf3c0e76ab7e07dcd504bf45eaf3a
jasonc1025 <jasonc@eande.world>
2018-04-02 20:01:48 -0700

    Customize 'filepicker' API Key

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './routes';
 
-filepicker.setKey('YOUR_API_KEY');
+// [jwc]+1 filepicker.setKey('YOUR_API_KEY');
+filepicker.setKey('AI1now8VTTlyQkRyFUn3bz');
 
 ReactDOM.render(
   Routes,
